9e82425d38001f40aa0bddb6e325907af699ac4a
Lucinde <lucinde@lucinde.nl>
2023-05-24 15:39:11 +0200

    Add data sql, Customer repository and dto

---
+++ src/main/java/com/lucinde/plannerpro/dtos/CustomerDto.java
@@ -0,0 +1,11 @@
+package com.lucinde.plannerpro.dtos;
+
+public class CustomerDto {
+    public Long id;
+    public String name;
+    public String address;
+    public String zip;
+    public String city;
+    public String phoneNumber;
+    public String email;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO customers (name, address, zip, city, phone_number, email)
+VALUES ('Pietje Puk', 'Lindelaan 31', '1234AB', 'Haarlem', '06-14567823', 'pietje@puk.nl');

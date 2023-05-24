ee5b384196826aec88416a7fdc85f37dac0151fa
Lucinde <lucinde@lucinde.nl>
2023-05-24 15:23:52 +0200

    Created model Customer

---
+++ src/main/java/com/lucinde/plannerpro/models/Customer.java
@@ -0,0 +1,23 @@
+package com.lucinde.plannerpro.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private String zip;
+    private String city;
+    private String phoneNumber;
+    private String email;
+
+
+}

e86eae8002c0d1c10b3ee323194f85e561fec0e5
Lucinde <lucinde@lucinde.nl>
2023-05-24 13:04:30 +0200

    First initialization project

---
+++ src/main/java/com/lucinde/plannerpro/PlannerproApplication.java
@@ -0,0 +1,13 @@
+package com.lucinde.plannerpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucinde/plannerpro/PlannerproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucinde.plannerpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

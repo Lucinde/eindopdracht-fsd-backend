45ce8c0a20502380f0b8b392287e3f7483f760c0
Lucinde <lucinde@lucinde.nl>
2023-06-02 09:19:20 +0200

    Create model ScheduleTask

---
+++ src/main/java/com/lucinde/plannerpro/models/ScheduleTask.java
@@ -0,0 +1,23 @@
+package com.lucinde.plannerpro.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "schedule_tasks")
+public class ScheduleTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+}

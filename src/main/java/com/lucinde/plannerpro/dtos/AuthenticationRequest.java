139c75d7c74f54e2d04a364d66a6c8eeeca9e575
Lucinde <lucinde@lucinde.nl>
2023-06-16 19:30:03 +0200

    Add AutenticationResponse and AuthenthicationRequest

---
+++ src/main/java/com/lucinde/plannerpro/dtos/AuthenticationRequest.java
@@ -0,0 +1,26 @@
+package com.lucinde.plannerpro.dtos;
+//Aangezien dit een soort Dto is deze toegevoegd aan dtos ipv payload
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+
+    public AuthenticationRequest() {
+    }
+    public AuthenticationRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

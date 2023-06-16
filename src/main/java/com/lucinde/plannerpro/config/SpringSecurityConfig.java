2a733c531c18e7d1f29812ce1c9a43624ae84309
Lucinde <lucinde@lucinde.nl>
2023-06-16 19:32:52 +0200

    Add JwtRequestFilter, GlobalCorsConfiguration and SpringSecurityConfiguration

---
+++ src/main/java/com/lucinde/plannerpro/config/SpringSecurityConfig.java
@@ -0,0 +1,86 @@
+package com.lucinde.plannerpro.config;
+
+import com.lucinde.plannerpro.filters.JwtRequestFilter;
+import com.lucinde.plannerpro.services.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    public final CustomUserDetailsService customUserDetailsService;
+    private final JwtRequestFilter jwtRequestFilter;
+    private final PasswordEncoder passwordEncoder;
+
+    public SpringSecurityConfig(CustomUserDetailsService customUserDetailsService, JwtRequestFilter jwtRequestFilter, PasswordEncoder passwordEncoder) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.jwtRequestFilter = jwtRequestFilter;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    // Authenticatie met customUserDetailsService en passwordEncoder
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder)
+                .and()
+                .build();
+    }
+
+
+//    // PasswordEncoderBean. Deze kun je overal in je applicatie injecteren waar nodig.
+//    // Je kunt dit ook in een aparte configuratie klasse zetten.
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+    // Authorizatie met jwt
+    @Bean
+    protected SecurityFilterChain filter (HttpSecurity http) throws Exception {
+
+        //JWT token authentication
+        http
+                .csrf().disable()
+                .httpBasic().disable()
+                .cors().and()
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                .requestMatchers(HttpMethod.GET,"/users").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST,"/users/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT,"/users/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
+                /*voeg de antmatchers toe voor admin(post en delete) en user (overige)*/
+                .requestMatchers(HttpMethod.POST, "/cimodules").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/cimodules/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/remotes").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/remotes/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/televisions").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/televisions/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/wallbrackets").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/wallbrackets/**").hasRole("ADMIN")
+                // Er kunnen meerdere paths in 1 request staan:
+                .requestMatchers("/cimodules", "/remotes", "/televisions", "/wallbrackets").hasAnyRole("ADMIN", "USER")
+                .requestMatchers("/authenticated").authenticated()
+                .requestMatchers("/authenticate").permitAll()/*allen dit punt mag toegankelijk zijn voor niet ingelogde gebruikers*/
+                .anyRequest().denyAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+}
